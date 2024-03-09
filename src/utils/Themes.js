2d1b454fbd27e497846e82f256ab6666faf6fa4d
vishpatel24 <vishpatel24@outlook.com>
2024-03-09 14:27:06 -0500

    init

---
+++ src/utils/Themes.js
@@ -0,0 +1,24 @@
+export const darkTheme = {
+    bg:"#1C1C27",
+    bgLight: "#1C1E27",
+    primary:"##4c59e6",
+    text_primary:"#002846",
+    text_secondary:"32383e",
+    card:"#171721",
+    card_light: '#191924',
+    button:"#854CE6",
+    white:"#FFFFFF",
+    black:"#000000",
+}
+
+export const lightTheme = {
+    bg:"#fdf8ed",
+    bgLight: "#fff2da",
+    primary:"#002846",
+    text_primary:"#01375f",
+    text_navbar:"#dce0e3",
+    text_hover:"#ffffff",
+    text_secondary:"32383e",
+    card:"#a8a0a0",
+    button:"#5c5b5b",
+}
